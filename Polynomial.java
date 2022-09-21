9f6eb47551e1cdd752e7c96aabb5c0cf32f96bd8
Rebecca Han <rebeccarh228@gmail.com>
2022-09-21 14:26:27 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,44 @@
+import java.lang.Math;
+
+public class Polynomial{
+	double[] coefficients;
+	
+	public Polynomial(){
+		coefficients = new double[1];
+		coefficients[0] = 0;
+	}
+
+	public Polynomial(double[] inputs){
+		coefficients = new double[inputs.length];
+		for(int i=0; i<inputs.length; i++){
+			coefficients[i] = inputs[i];
+		}
+	}
+
+	public Polynomial add(Polynomial poly){
+		for(int i=0; i<coefficients.length; i++){
+			poly.coefficients[i] += coefficients[i];
+		}
+
+		return poly;
+	} 
+
+	public double evaluate(double x){
+		double ans = 0;
+		
+		for(int i=0; i<coefficients.length; i++){
+			ans += coefficients[i]*Math.pow(x,i);
+		}
+
+		return ans;
+	}
+
+	public boolean hasRoot(double x){
+		double ans = evaluate(x);
+
+		if (ans==0){
+			return true;
+		}
+		return false;
+	}
+}
